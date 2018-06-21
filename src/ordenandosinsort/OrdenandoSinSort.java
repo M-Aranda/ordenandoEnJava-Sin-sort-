041d9b55b4247035cac83cf10038dd3459dfc626
M-Aranda <cheloz_20@hotmail.com>
2018-06-21 00:35:16 -0300

    solucionado y creo que tambien explicado

--- src/ordenandosinsort/OrdenandoSinSort.java
+++ src/ordenandosinsort/OrdenandoSinSort.java
@@ -1,17 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ordenandosinsort;
 
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- *
- * @author Marce
- */
 public class OrdenandoSinSort {
 
     /**
@@ -19,48 +10,64 @@ public class OrdenandoSinSort {
      */
     public static void main(String[] args) {
 
-        List<Integer> numeros = new ArrayList();
+        List<Integer> numeros = new ArrayList(); //crear nueva lista de integers.
 
         numeros.add(-3);
         numeros.add(6);
         numeros.add(10);
-        numeros.add(5);
+        numeros.add(55);
         numeros.add(2);
-        numeros.add(-9);
-        
+        numeros.add(-33);
+        //las 6 lineas anteriores agregan numeros
 
-        // busco que devuuelva 10, 6, 5, 2, -3, -9
-        boolean hayCambios = true;
 
-        while (hayCambios) {
 
-            for (int i = 0; i < numeros.size(); i++) {
+        int a=0;//una variable en donde pueda guardar el valor del indice antes de cambiarlo
 
-                if (i + 1 < numeros.size()) {
-
-                    if (numeros.get(i) < numeros.get(i + 1)) {
-
-                        int n1 = numeros.get(i);
-                        int n2 = numeros.get(i + 1);
-
-                        numeros.set(i, n2);
-                        numeros.set(i + 1, n1);
-                        hayCambios = true;
-                    } else if (numeros.get(i) > numeros.get(i + 1)) {
-                        hayCambios = false;
-                    }
-
-                }
-
+        for (int i = 0; i < numeros.size(); i++) { //ciclo superior ejecuta un ciclo por cada elemento en la lista
+            for (int j = 1; j < numeros.size()-1; j++) {//ciclo interior que compara cada valor
+                if((numeros.get(j-1))< (numeros.get(j))){// if que determina si el valor del indice actual, de este ciclo es mayor al siguiente
+                    a=numeros.get(j-1);//variable guarda valor de variable 
+                    numeros.set(j-1,numeros.get(j));//cambiar el primero por el segundo
+                    numeros.set(j,a);//darle al segundo indice, el valor del primero
+                
             }
-
+                
+            }
+            
+            
+            
+        }
+        
+        for (Integer i : numeros) {// ciclo for para mostrar los numeros ya ordenados
+            System.out.println(i);
+            
         }
 
-
-        for (Integer numero : numeros) {
-            System.out.println(numero);
-
-        }
 
     }
+
+//    private static void bubbleSort(int[] arr) {
+//       
+//            int n = arr.length;
+//            int temp = 0;
+//            for (int i = 0; i < n; i++) {
+//                for (int j = 1; j < (n - i); j++) {
+//                    if (arr[j - 1] < arr[j]) {
+//                        //swap elements  
+//                        temp = arr[j - 1];
+//                        arr[j - 1] = arr[j];
+//                        arr[j] = temp;
+//                    
+//        
+//
+//    }
+    
+    
+
+    
 }
+            
+    
+    
+
